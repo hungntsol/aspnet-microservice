bd42cf75971ea8a8ac315711a837f60015f680dc
Hung Nguyen <gaughegomth01@gmail.com>
2022-07-06 11:00:55 +0700

    transactional attribute

---
+++ src/Services/Order/Order.Application/Attributes/TransactionalAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.Transactions;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Order.Application.Attributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Interface)]
+public class TransactionalAttribute : Attribute, IAsyncActionFilter
+{
+	public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+	{
+		using var transactionScope = new TransactionScope();
+		var actionExecutedContext = await next();
+
+		if (actionExecutedContext.Exception is null)
+		{
+			transactionScope.Complete();
+		}
+	}
+}
